d318fc1e4965edab57ab6e97a1e50b20ea7d7b61
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2022-02-09 21:53:31 +0300

    feat: add solve of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let acc = 0;
+  let brackets = {};
+  bracketsConfig.forEach(item => brackets[item[0]] = item[1]);
+  let arr = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    acc = str[i] === arr[arr.length - 1] ? acc - 1 : acc + 1;
+    str[i] === arr[arr.length - 1] ? arr.pop() : arr.push(brackets[str[i]]);
+  }
+  
+  console.log(arr)
+  
+  return acc === 0;
+};
